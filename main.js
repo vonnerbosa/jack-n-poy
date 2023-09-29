57a0139419220cbb45ccbb824ed2c006a624286a
vonnerbosa <vonherbosa@gmail.com>
2023-09-29 00:45:01 -0700

    added js and css file

---
+++ main.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    ranNum = Math.floor(Math.random() * 3);
+
+    if (ranNum = 0) {
+        return "Rock"
+    }
+
+    else if (ranNum = 1) {
+        return "Paper"
+    }
+
+    else {
+        return "Scissors"
+    }
+}
