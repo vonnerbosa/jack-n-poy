25c55ff902f2b5a5ac5a0b9d182b5a5f7884660c
vonnerbosa <vonherbosa@gmail.com>
2023-10-12 12:22:00 -0700

    restored git branch, files were done a week ago

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
-function getComputerChoice() {
-    ranNum = Math.floor(Math.random() * 3);
-
-    if (ranNum = 0) {
-        return "Rock"
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection = computerSelection) {
+       return "It's a draw!";
     }
 
-    else if (ranNum = 1) {
-        return "Paper"
+    else if (playerSelection = "rock" && computerSelection = "paper") {
+        return "You Lose! Paper beats Rock";
+    }
+
+    else if (playerSelection = "rock" && computerSelection = "scissors") {
+        return "You Win! Rock beats Scissors!"
+    }
+    
+   
+  }
+   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function getComputerChoice() {
+    ranNum = Math.floor(Math.random() * 3);
+    
+    if (ranNum == 0) {
+        return "rock"
+    }
+
+    else if (ranNum == 1) {
+        return "paper"
     }
 
     else {
-        return "Scissors"
+        return "scissors"
     }
 }
+
